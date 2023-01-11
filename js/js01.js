06a3a80d57608bf5c9f517542d208776a0d591d6
RaygozaRafael <raygoza.zamudio.rafael@gmail.com>
2023-01-11 13:48:28 -0600

    CI: Converion de tipos de datos

--- js/js01.js
+++ js/js01.js
@@ -1,1 +1,19 @@
-/*  */console.log("Sesión js01");
+console.log("Sesión js01");
+/* 
+Tipos de datos primitivos en JavaScript
+-string ("Hola numero")
+-number (8008 | 8080.819)
+    Utiliza 64 bits para almacenar la variable, mas solo 53 son usados para el valor del numero
+-bigint (8008n)
+    convertir un dato numerico a bigint | let bigIntVar = BigInt(maxNumber);
+-boolean
+-undefined
+-symbol
+
+Tipos de dato objetos
+-Object
+-Array
+
+Funciones
+
+*/
